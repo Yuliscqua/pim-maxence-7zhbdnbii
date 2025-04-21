4cb58ce2aa7e24893d0e88f8dd39afc3e7785f06
Yuliscqua <m_rannou4@etu-webschoolfactory.fr>
2025-04-21 18:06:51 +0200

    Ajout des styles pour les 3 premières pages

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Home () {
     return (
         <div className="Home">
             <div className="logo">INDIE<span className="link-text">LINK</span></div>
-            <p>Bienvenue sur IndieLink ! Le site qui rassemble tous les amateurs de jeux vidéo indépendants ! Pour profiter de notre expérience en communauté, commencez à vous inscrire ou connectez-vous ! </p>
+            <p className="home-text">Bienvenue sur IndieLink ! Le site qui rassemble tous les amateurs de jeux vidéo indépendants ! Pour profiter de notre expérience en communauté, commencez à vous inscrire ou connectez-vous ! </p>
             <Link className="signin" to="/signin">S'inscrire</Link>
             <Link className="login" to="/login">Se connecter</Link>
         </div>
