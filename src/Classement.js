c19616d43e8e4c21eaa7daf8f747b4d73a60e631
Yuliscqua <m_rannou4@etu-webschoolfactory.fr>
2025-04-22 02:36:43 +0200

    Encore plus de Madeline

--- src/Classement.js
+++ src/Classement.js
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { doc, getDocs, updateDoc, arrayUnion, arrayRemove, collection, setDoc } from 'firebase/firestore';
 import { db, auth } from './firebase';
-import Header from './components/Header.js'
+import Header from './components/Header.js';
+import SearchBar from './components/SearchBar.js';
+import './style.css';
 
 function Classement () {
     const { userId } = useParams();
     
     return (
-        <Header userId={userId} />
+        <div>
+            <Header userId={userId} />
+            <h2 className="beta-title">Classement</h2>
+            <SearchBar/>
+        </div>
     )
 }
 
